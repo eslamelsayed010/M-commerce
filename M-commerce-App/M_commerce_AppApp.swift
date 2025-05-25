78df064981ac7db9f981d8306e13868a638a1e20
ahmedabdeen74 <ahmedabdeen1408@gmail.com>
2025-05-25 22:06:19 +0300

    Initial Commit

---
+++ M-commerce-App/M_commerce_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  M_commerce_AppApp.swift
+//  M-commerce-App
+//
+//  Created by mac on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct M_commerce_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
