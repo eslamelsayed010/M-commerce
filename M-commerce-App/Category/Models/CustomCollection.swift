3c6f7db30291d3659cf65ecb6ffe9c8520b0e0f4
AmanyAdel2 <amanyadel20242024@gmail.com>
2025-06-13 19:58:46 +0300

    category model

---
+++ M-commerce-App/Category/Models/CustomCollection.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollection.swift
+//  M-commerce-App
+//
+//  Created by Macos on 12/06/2025.
+//
+
+import Foundation
+struct CustomCollection: Identifiable {
+    let id: Int
+    let title: String
+    let handle: String
+}
