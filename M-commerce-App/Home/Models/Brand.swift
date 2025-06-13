c5824b93969b570ccbb46ea0fb67e02b7f13f79f
AmanyAdel2 <amanyadel20242024@gmail.com>
2025-06-13 19:51:38 +0300

    Home Models

---
+++ M-commerce-App/Home/Models/Brand.swift
@@ -0,0 +1,15 @@
+//
+//  Brand.swift
+//  M-commerce-App
+// by Amany
+//  Created by Macos on 04/06/2025.
+//
+
+import Foundation
+
+
+struct Brand: Identifiable {
+    let id = UUID()
+    let name: String
+    let imageUrl: String?
+}
---
+++ M-commerce-App/Home/Models/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  M-commerce-App
+//
+//  Created by Macos on 09/06/2025.
+//
+
+import Foundation
+struct Product: Identifiable {
+    let id : String
+    let title: String
+    let imageUrl: String?
+    let price: Double?
+    let currencyCode: String?
+    let productType: String?
+}
