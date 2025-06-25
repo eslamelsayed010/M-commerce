ae167000843032f7b471487fa98a915951e4685a
AmanyAdel2 <amanyadel20242024@gmail.com>
2025-06-25 22:56:52 +0300

    orderHistorye

---
+++ M-commerce-App/Features/Order/Views/OrderDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderDetailView.swift
+//  M-commerce-App
+//
+//  Created by Macos on 25/06/2025.
+//
+
+import SwiftUI
+
+struct OrderDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetailView()
+    }
+}
